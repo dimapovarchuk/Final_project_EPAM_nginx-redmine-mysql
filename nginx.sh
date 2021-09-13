58394c2349cc476ec03d8844ae12166fa6793982
Dima Povarchuk <52627259+dimapovarchuk@users.noreply.github.com>
2021-09-13 18:07:48 +0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+sudo apt update -y
+sudo apt install nginx -y
 sudo apt-get -o Acquire::ForceIPv4=true update -y
 sudo apt-get -o Acquire::ForceIPv4=true upgrade -y
+echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
 
 sudo cat <<EOF > /etc/nginx/sites-available/default
+
 upstream web_backend {
         server 10.0.1.32:3000;
         server 10.0.1.33:3000;
@@ -13,6 +17,7 @@ location / {
 proxy_pass http://web_backend;
 }
 }
+
 EOF
 
 sudo service nginx restart
